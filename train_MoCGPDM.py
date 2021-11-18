056a0e006cfa7de216bab15e7c9c9ae87b377e6e
cexuzheng <64259990+cexuzheng@users.noreply.github.com>
2021-11-18 10:22:24 +0100

    Update train_MoCGPDM.py

--- train_MoCGPDM.py
+++ train_MoCGPDM.py
@@ -56,11 +56,13 @@ param_dict['u_dim'] = u_dim
 param_dict['N_max'] = 450
 model = MoGPDM(**param_dict)
 
-	# set some pre-trained weights
+	# set some pre-trained weights for a better training performance (you may need other weights for different set of data)
 
 model.map_cluster_kernel_length = torch.nn.Parameter(torch.tensor([27.7606, 23.0840, 21.5175, 20.5224, 20.5270] ,dtype=model.dtype, device=model.device))
 model.latent_cluster_kernel_length = torch.nn.Parameter(torch.tensor([73.0439, 44.8699, 49.7291, 34.5981, 35.1434, 72.5992, 45.6354, 50.0329,
         34.9401, 35.0878, 20.0000, 20.1708, 20.0671, 20.0000, 20.1708, 20.0671],dtype=model.dtype, device=model.device))
+
+    # set the training set
 
 training_set = np.random.choice(len(Y_names), size=num_data, replace=False)
 print('training_set is ', training_set)
@@ -73,13 +75,17 @@ for i in training_set.astype(int):
     Y_data = np.loadtxt(Y_names[i], delimiter = ',')
     U_data = np.loadtxt(U_names[i], delimiter = ',')
     n = Y_data.shape[0]
-    	# 	commet the following line if you want to train it with the whole size
+
+    	# 	comment the following line if you want to train it with the full size
+        #   instead of the reduced(ore concentrated) version.
+
     Y_data = (Y_data.reshape(n,64,3)-Y_data[:,(189, 190, 191)].reshape(n, 1, 3)).reshape(n,192)
 
     model.add_data(Y_data,U_data)
 
 
-# init model
+# init model makes a few trainings to search for stable configurations (sometimes it finds singular kernels)
+
 keep_trying = True
 init_loss = 0
 while keep_trying:
@@ -97,7 +103,7 @@ while keep_trying:
     except:
         pass
 
-        # 	start the training
+    # 	start the training
 
 loss_tracking = [init_loss]
 Y = torch.tensor(model.get_Y(), dtype = model.dtype, device = model.device)
@@ -117,7 +123,7 @@ print(loss_tracking)
 for i in range(15):
     print('i = ', i)
     try:
-    		#	trein with the adam optimizer
+    		#	train with the adam optimizer
         loss = model.train_experts(num_opt_steps = 50, num_print_steps=10, lr=0.01, balance=1, train_X = True, optimizer_type = 'adam')
         loss_tracking.append(model.gpdm_loss(Y,N,1).detach().numpy())
         print(loss_tracking)
@@ -129,7 +135,8 @@ for i in range(15):
         np.savetxt(save_folder+'loss_tracking.csv', np.array(loss_tracking), delimiter=',')
         model.init_means()
     except:
-        # load log_dict
+            #   failed to train, reload model
+            #   load log_dict
         log_dict = pickle.load(open(save_folder+'log_dict.pt', 'rb' ) )
         # init GPDM object
         D = log_dict['D']
